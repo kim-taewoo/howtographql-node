24c0401f2f0edfb3f0f0974b9c0eaaa20067a448
kim-taewoo <acoustic0419@gmail.com>
2020-11-04 01:59:10 +0900

    간단한 graphql 인증과정 구현

--- src/index.js
+++ src/index.js
@@ -1,32 +1,15 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { PrismaClient } = require('@prisma/client');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const User = require('./resolvers/User');
+const Link = require('./resolvers/Link');
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: async (parent, args, context) => {
-      return context.prisma.link.findMany()
-    },
-  },
-  Mutation: {
-    post: (parent, args, context, info) => {
-      const newLink = context.prisma.link.create({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      })
-      return newLink;
-    },
-  },
-
-  // GraphQL 스키마 내의 Link 타입을 위한 추가적인 resolvers.
-  // 그런데 아래처럼 아주 단순한 경우에는 생략해도 알아서 된다.
-  // Link: {
-  //   id: (parent) => parent.id,
-  //   description: (parent) => parent.description,
-  //   url: (parent) => parent.url,
-  // }
+  Query,
+  Mutation,
+  User,
+  Link,
 };
 
 const prisma = new PrismaClient();
@@ -34,8 +17,11 @@ const prisma = new PrismaClient();
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
-  context: {
-    prisma
+  context: request => {
+    return {
+      ...request,
+      prisma
+    }
   }
 });
 
