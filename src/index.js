9716c5f1178a47727f039825fb50051635d4401f
kim-taewoo <acoustic0419@gmail.com>
2020-11-03 21:03:21 +0900

    prisma client 서버와 첫 연결

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  },
-]
-
-let idCount = links.length;
+const { PrismaClient } = require('@prisma/client');
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany()
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      }
-      links.push(link);
-      return link;
-    }
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      })
+      return newLink;
+    },
   },
 
   // GraphQL 스키마 내의 Link 타입을 위한 추가적인 resolvers.
-  // 그런데 아래처럼 아주 단순한 경우에는 생략해도 알아서 된다. 
+  // 그런데 아래처럼 아주 단순한 경우에는 생략해도 알아서 된다.
   // Link: {
   //   id: (parent) => parent.id,
   //   description: (parent) => parent.description,
@@ -36,9 +29,14 @@ const resolvers = {
   // }
 };
 
+const prisma = new PrismaClient();
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: {
+    prisma
+  }
 });
 
 server.start(() => console.log('Server is running'));
