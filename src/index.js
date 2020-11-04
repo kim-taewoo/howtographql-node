146e37d8b1b3ac36bf5c9cd3432cea354a5e259c
kim-taewoo <acoustic0419@gmail.com>
2020-11-04 19:22:10 +0900

    graphQL subscription 을 간단 구현 및 테스트

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-const { GraphQLServer } = require('graphql-yoga');
+const { GraphQLServer, PubSub } = require('graphql-yoga');
 const { PrismaClient } = require('@prisma/client');
 const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Vote = require('./resolvers/Vote')
+const Subscription = require('./resolvers/Subscription')
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
 
 const prisma = new PrismaClient();
+const pubsub = new PubSub();
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
@@ -20,7 +25,8 @@ const server = new GraphQLServer({
   context: request => {
     return {
       ...request,
-      prisma
+      prisma,
+      pubsub,
     }
   }
 });
